cbe97a643400f70cdf4b66a66b4948251b05bd26
Aidan Y <157079558+aidan-yip@users.noreply.github.com>
2024-02-13 20:00:09 -0800

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ const monsters = [
 const locations = [
   {
     name: "town square",
-    "button text": ["Go to store", "Go to cave", "Fight Ender dragon"],
+    "button text": ["Go to store", "Go to cave", "Fight dragon"],
     "button functions": [goStore, goCave, fightDragon],
     text: "You are in the town square. You see a sign that says \"Shop\". Enter?"
   },
