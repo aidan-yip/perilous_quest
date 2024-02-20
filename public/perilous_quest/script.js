74b079b9b8c7744e3481a9420b6dbdde692ef0ea
Aidan Y <157079558+aidan-yip@users.noreply.github.com>
2024-02-20 14:27:37 -0800

    Update script.js

--- public/perilous_quest/script.js
+++ public/perilous_quest/script.js
@@ -1,9 +1,3 @@
-// Beta alert box
-
-window.onload = function(){ 
-  alert("Welcome to Perilous Quest! This game is in alpha and is not yet ready for release. Please enjoy the game and report any bugs you see!");
-}
-
 // Variables
 
 let xp = 0;
