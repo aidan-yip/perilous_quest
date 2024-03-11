3fb3bdfb427042c2f6e044fa690b8a37e799f344
Aidan Y <157079558+aidan-yip@users.noreply.github.com>
2024-03-11 14:37:34 -0700

    Update game_autoplay.js

--- public/perilous_quest/game_autoplay.js
+++ public/perilous_quest/game_autoplay.js
@@ -1,2 +1,10 @@
+// Town Music autoplay
+
 let lovely = document.getElementById("lovely_town");
 lovely.play();
+
+// Beta alert box
+
+window.onload = function(){
+   alert("Welcome to Perilous Quest! This game is in Beta and is not yet ready for release. Please enjoy the game and report any bugs you see!");
+}
