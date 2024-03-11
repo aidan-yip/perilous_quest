25b7affd85d080e90df77d4770b85c58f44af401
Aidan Y <157079558+aidan-yip@users.noreply.github.com>
2024-03-11 14:38:01 -0700

    Update app_start.js

--- app_start.js
+++ app_start.js
@@ -1,5 +1,44 @@
-// Beta alert box
+// Notifications
 
-window.onload = function(){ 
-    alert("Welcome to Perilous Quest! This game is in Beta and is not yet ready for release. Please enjoy the game and report any bugs you see!");
+window.onload = function(){
+    Notification.requestPermission(). then (perm => {
+      if (perm === "granted") {
+        new Notification("Perilous Quest", {
+          body: "For the best in game experience please use a desktop",
+          icon: "homescreen192.png",
+          tag: "desktop",
+          vibrate: [200, 100, 200], 
+        })
+      }
+  })
   }
+
+const button = document.querySelector ("button")
+
+button.addEventListener("click", () => {
+    Notification.requestPermission(). then (perm => {
+        if (perm === "granted") {
+          new Notification("Perilous Quest", {
+            body: "For the best in game experience please use a desktop",
+            icon: "homescreen192.png", 
+            tag: "desktop",
+            vibrate: [200, 100, 200], 
+          })
+        }
+    })
+})
+
+let notification
+
+document.addEventListener ("visibilitychange", () => {
+    if (document.visibilityState === "hidden") {
+      notification = new Notification ("Perilous Quest", { 
+        body: "Perilous Quest is still running. To stop the audio close the app/webpage",
+        icon: "homescreen192.png",
+        tag: "Come Back",
+      })
+    } else {
+        notification.close()
+    }
+
+})
