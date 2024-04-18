eccbaaf2750563c592b0972e650b27ecc9d04f1a
Aidan Y <157079558+aidan-yip@users.noreply.github.com>
2024-04-18 15:53:50 -0700

    Update app_start.js

--- app_start.js
+++ app_start.js
@@ -1,28 +1,30 @@
 // Clock
 
 function currentTime() {
-  let date = new Date(); 
+  let date = new Date();
   let hh = date.getHours();
   let mm = date.getMinutes();
   let ss = date.getSeconds();
   let session = "AM";
 
-  if(hh == 0){
-      hh = 12;
+  if (hh == 0) {
+    hh = 12;
   }
-  if(hh > 12){
-      hh = hh - 12;
-      session = "PM";
-   }
+  if (hh > 12) {
+    hh = hh - 12;
+    session = "PM";
+  }
 
-   hh = (hh < 10) ? "0" + hh : hh;
-   mm = (mm < 10) ? "0" + mm : mm;
-   ss = (ss < 10) ? "0" + ss : ss;
-    
-   let time = hh + ":" + mm + ":" + ss + " " + session;
+  hh = hh < 10 ? "0" + hh : hh;
+  mm = mm < 10 ? "0" + mm : mm;
+  ss = ss < 10 ? "0" + ss : ss;
 
-  document.getElementById("clock").innerText = time; 
-  let t = setTimeout(function(){ currentTime() }, 1000);
+  let time = hh + ":" + mm + ":" + ss + " " + session;
+
+  document.getElementById("clock").innerText = time;
+  let t = setTimeout(function () {
+    currentTime();
+  }, 1000);
 }
 
 currentTime();
@@ -32,51 +34,63 @@ currentTime();
 const date = new Date();
 const dateWithoutTime = date.toDateString();
 
-    document.getElementById("date").innerHTML = "Hey there! Today is " + dateWithoutTime;
-
+document.getElementById("date").innerHTML =
+  "Hey there! Today is " + dateWithoutTime;
 
 // Notifications
 
-
 //Button selector link to Notifications
 
-const button = document.getElementById("startbutton")
+const button = document.getElementById("startbutton");
 
 button.addEventListener("click", () => {
-    Notification.requestPermission(). then (perm => {
-        if (perm === "granted") {
-          new Notification("Game Alert", {
-            body: "For the best in game experience please use a desktop",
-            icon: "./public/icons/homescreen192.png", 
-            tag: "desktop",
-            vibrate: [200, 100, 200], 
-          })
-        }
-    })
-})
+  Notification.requestPermission().then((perm) => {
+    if (perm === "granted") {
+      new Notification("Game Alert", {
+        body: "For the best in game experience please use a desktop",
+        icon: "./public/icons/homescreen192.png",
+        tag: "desktop",
+        vibrate: [200, 100, 200],
+      });
+    }
+  });
+});
+
+//audio hover
+
+let selectfx = document.getElementById("selectfx");
+let sharebutton = document.getElementById("sharebutton");
+let startbutton = document.getElementById("startbutton");
+
+startbutton.onclick = visitPage;
+sharebutton.onclick = playmusic;
+
+function playmusic() {
+  selectfx.currentTime = 0;
+  selectfx.play();
+}
 
 //Music Alert trigger when window not focused
 
-let notification
+let notification;
 
-document.addEventListener ("visibilitychange", () => {
-    if (document.visibilityState === "hidden") {
-      notification = new Notification ("Music Alert", { 
-        body: "Perilous Quest is still running. To stop the audio close the app",
-        icon: "./public/icons/homescreen192.png",
-        tag: "Come Back",
-      })
-    } else {
-        notification.close()
-    }
+document.addEventListener("visibilitychange", () => {
+  if (document.visibilityState === "hidden") {
+    notification = new Notification("Music Alert", {
+      body: "Perilous Quest is still running. To stop the audio close the app",
+      icon: "./public/icons/homescreen192.png",
+      tag: "Come Back",
+    });
+  } else {
+    notification.close();
+  }
+});
 
-})  
+//Click to start button (switch page)
 
-//Click to start button
-
-    function visitPage(){
-        window.location='./public/perilous_quest/index.html';
-    }
+function visitPage() {
+  window.location = "./public/perilous_quest/index.html";
+}
 
 //Share button
 
@@ -101,45 +115,63 @@ btn.addEventListener("click", async () => {
 
 //Cursor trail
 
-(function($) {
-
+(function ($) {
   var baseCssClass = "cursor-trail",
-      addPoint = function(pageX, pageY, cssClass, timeToGrow, timeToShrink, scale) {
-          // Create a new point located at the mouse position
-          var point = $("<div>", {
-              "class": cssClass,
-              css: {
-                  left: pageX,
-                  top: pageY
-              }
-          }).appendTo('body');
+    addPoint = function (
+      pageX,
+      pageY,
+      cssClass,
+      timeToGrow,
+      timeToShrink,
+      scale
+    ) {
+      // Create a new point located at the mouse position
+      var point = $("<div>", {
+        class: cssClass,
+        css: {
+          left: pageX,
+          top: pageY,
+        },
+      }).appendTo("body");
 
-          // now make the point grow, then shrink and finally disappear
-          point
-              .transition({ scale: scale }, timeToGrow)
-              .transition({ scale: 1 }, timeToShrink, function() { point.remove(); });
-      };
+      // now make the point grow, then shrink and finally disappear
+      point
+        .transition({ scale: scale }, timeToGrow)
+        .transition({ scale: 1 }, timeToShrink, function () {
+          point.remove();
+        });
+    };
 
-  $.fn.cursorTrail = function(options) {
-      // assign defaults for those options not supplied
-      options = $.extend({
-          timeToGrow: 200,
-          timeToShrink: 200,
-          scale: 1,
-          "class": ""
-      }, options);
+  $.fn.cursorTrail = function (options) {
+    // assign defaults for those options not supplied
+    options = $.extend(
+      {
+        timeToGrow: 200,
+        timeToShrink: 200,
+        scale: 1,
+        class: "",
+      },
+      options
+    );
 
-      // add the base css class all cursor trail points need.
-      var actualCssClass = baseCssClass;
-      if (options["class"]) {
-          actualCssClass += " " + options["class"];
-      }
+    // add the base css class all cursor trail points need.
+    var actualCssClass = baseCssClass;
+    if (options["class"]) {
+      actualCssClass += " " + options["class"];
+    }
 
-      return this.bind("mousemove", function(ev) {
-          addPoint(ev.pageX, ev.pageY, actualCssClass, options.timeToGrow, options.timeToShrink, options.scale);
-      });
+    return this.bind("mousemove", function (ev) {
+      addPoint(
+        ev.pageX,
+        ev.pageY,
+        actualCssClass,
+        options.timeToGrow,
+        options.timeToShrink,
+        options.scale
+      );
+    });
   };
-}(jQuery));
+})(jQuery);
 
 // jQuery.transit has a bug in older IE versions, so switch to jQuery animate
 if (!$.support.transition) {
@@ -147,7 +179,5 @@ if (!$.support.transition) {
 }
 
 $("#background").cursorTrail({
-  "class": "yellow-trail"
+  class: "yellow-trail",
 });
-
-
