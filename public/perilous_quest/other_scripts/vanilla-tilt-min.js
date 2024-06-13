b3abee010506eed8393c98686c23bc34ad1ae9eb
Aidan Y <jamesayipt6@gmail.com>
2024-06-12 19:39:19 -0700

    Reduced file size for images
    Reduced file size for images

--- public/perilous_quest/other_scripts/vanilla-tilt-min.js
+++ public/perilous_quest/other_scripts/vanilla-tilt-min.js
@@ -1,3 +1,10 @@
+/**
+ * Created by Sergiu Șandor (micku7zu) on 1/27/2017.
+ * Original idea: https://github.com/gijsroge/tilt.js
+ * MIT License.
+ * Version 1.8.1
+ */
+
 let VanillaTilt = (function () {
   "use strict";
   class t {
