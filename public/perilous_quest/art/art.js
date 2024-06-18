2628455a410a43373adb4f4a2903e8719548dcd8
Aidan Y <jamesayipt6@gmail.com>
2024-06-17 22:44:40 -0700

    Added music to art page
    Added music to art page

--- public/perilous_quest/art/art.js
+++ public/perilous_quest/art/art.js
@@ -1,64 +1,84 @@
 // Offline Alert
 
-window.addEventListener('offline', function(event){
+window.addEventListener("offline", function (event) {
   alert("You're offline please reconnect to Wi-Fi to play Perilous Quest.");
 });
 
-
 //Cursor trail
 
-(function($) {
+(function ($) {
+  var baseCssClass = "cursor-trail",
+    addPoint = function (
+      pageX,
+      pageY,
+      cssClass,
+      timeToGrow,
+      timeToShrink,
+      scale
+    ) {
+      // Create a new point located at the mouse position
+      var point = $("<div>", {
+        class: cssClass,
+        css: {
+          left: pageX,
+          top: pageY,
+        },
+      }).appendTo("body");
 
-    var baseCssClass = "cursor-trail",
-        addPoint = function(pageX, pageY, cssClass, timeToGrow, timeToShrink, scale) {
-            // Create a new point located at the mouse position
-            var point = $("<div>", {
-                "class": cssClass,
-                css: {
-                    left: pageX,
-                    top: pageY
-                }
-            }).appendTo('body');
-  
-            // now make the point grow, then shrink and finally disappear
-            point
-                .transition({ scale: scale }, timeToGrow)
-                .transition({ scale: 1 }, timeToShrink, function() { point.remove(); });
-        };
-  
-    $.fn.cursorTrail = function(options) {
-        // assign defaults for those options not supplied
-        options = $.extend({
-            timeToGrow: 200,
-            timeToShrink: 200,
-            scale: 1,
-            "class": ""
-        }, options);
-  
-        // add the base css class all cursor trail points need.
-        var actualCssClass = baseCssClass;
-        if (options["class"]) {
-            actualCssClass += " " + options["class"];
-        }
-  
-        return this.bind("mousemove", function(ev) {
-            addPoint(ev.pageX, ev.pageY, actualCssClass, options.timeToGrow, options.timeToShrink, options.scale);
+      // now make the point grow, then shrink and finally disappear
+      point
+        .transition({ scale: scale }, timeToGrow)
+        .transition({ scale: 1 }, timeToShrink, function () {
+          point.remove();
         });
     };
-  }(jQuery));
-  
-  // jQuery.transit has a bug in older IE versions, so switch to jQuery animate
-  if (!$.support.transition) {
-    $.fn.transition = $.fn.animate;
-  }
-  
-  $("#background").cursorTrail({
-    "class": "blue-trail"
-  });
 
-//Navbar 
+  $.fn.cursorTrail = function (options) {
+    // assign defaults for those options not supplied
+    options = $.extend(
+      {
+        timeToGrow: 200,
+        timeToShrink: 200,
+        scale: 1,
+        class: "",
+      },
+      options
+    );
 
-window.onscroll = function() {scrollFunction()};
+    // add the base css class all cursor trail points need.
+    var actualCssClass = baseCssClass;
+    if (options["class"]) {
+      actualCssClass += " " + options["class"];
+    }
+
+    return this.bind("mousemove", function (ev) {
+      addPoint(
+        ev.pageX,
+        ev.pageY,
+        actualCssClass,
+        options.timeToGrow,
+        options.timeToShrink,
+        options.scale
+      );
+    });
+  };
+})(jQuery);
+
+// jQuery.transit has a bug in older IE versions, so switch to jQuery animate
+if (!$.support.transition) {
+  $.fn.transition = $.fn.animate;
+}
+
+$("#background").cursorTrail({
+  class: "blue-trail",
+});
+
+//Navbar
+
+window.onscroll = function () {
+  scrollFunction();
+  play_art();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
@@ -66,7 +86,14 @@ function scrollFunction() {
   } else {
     document.getElementById("navbar").style.backgroundColor = "transparent";
   }
+}
 
+//Play art music
+
+function play_art() {
+  let art = document.getElementById("art");
+  art.play();
+  console.log("Played art");
 }
 
 //Mobile menu
@@ -81,11 +108,11 @@ function show_menu() {
   if (list_menu.style.display === "flex") {
     list_menu.style.display = "none";
     menu_button_span.innerText = "menu";
-    menu_button.setAttribute("title", "Menu")
+    menu_button.setAttribute("title", "Menu");
   } else {
     list_menu.style.display = "flex";
     menu_button_span.innerText = "close";
-    menu_button.setAttribute("title", "Close")
+    menu_button.setAttribute("title", "Close");
   }
 }
 
@@ -96,5 +123,13 @@ const info_alert = document.getElementById("info_alert");
 info_alert.onclick = info_pop;
 
 function info_pop() {
-  alert("Perilous Quest" + "\n\n" + "Version 1.0.0" + "\n\n" + "(Beta build 9.9.0)" + "\n\n" + "Copyright © 2024 Aidan Yip. All rights reserved.");
+  alert(
+    "Perilous Quest" +
+      "\n\n" +
+      "Version 1.0.0" +
+      "\n\n" +
+      "(Beta build 9.9.0)" +
+      "\n\n" +
+      "Copyright © 2024 Aidan Yip. All rights reserved."
+  );
 }
